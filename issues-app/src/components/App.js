3e5813fadf9f51eae27696efcc052f76036c88fe
Shira-Meirovitz <shirameirovi@gmail.com>
2021-03-17 14:54:02 +0200

    array of issues

--- issues-app/src/components/App.js
+++ issues-app/src/components/App.js
@@ -1,15 +1,60 @@
 import React from "react";
 import "../App.css";
-import IssueList from "./IssueList";
-import Issue from "./Issue";
+import IssueList from "./components/IssueList";
+
+const issues = [
+  {
+    title: "My first issue",
+    id: 6,
+    timeCreated: "5 hours ago",
+    userNames: "noavrd",
+    Tags: "",
+    numOfComments: 2,
+  },
+  {
+    title: "How to add an HTML tag",
+    id: 5,
+    timeCreated: "21 hours ago",
+    userNames: "inbarsh",
+    Tags: "issue: bug report",
+    numOfComments: 0,
+  },
+  {
+    title: "npm start is not working",
+    id: 4,
+    timeCreated: "1 day ago",
+    userNames: "ariel",
+    Tags: "issue: need intervagation",
+    numOfComments: 5,
+  },
+  {
+    title: "Can`t start react app",
+    id: 3,
+    timeCreated: "3 days ago",
+    userNames: "aladin",
+    Tags: "issue: bug report",
+    numOfComments: 0,
+  },
+  {
+    title: "Saving the same builds from diffrent paths",
+    id: 2,
+    timeCreated: "7 days ago",
+    userNames: "wallas",
+    Tags: "needs trage",
+    numOfComments: 8,
+  },
+  {
+    title: "Debug extreamly slow",
+    id: 1,
+    timeCreated: "14 days ago",
+    userNames: "paak",
+    Tags: "needs trage",
+    numOfComments: 1,
+  },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <IssueList />
-      <Issue />
-    </div>
-  );
+  return <IssueList issue={issues} />;
 }
 
 export default App;
